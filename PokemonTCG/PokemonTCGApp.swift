e117532c2ac4ee8efcd56ad695af752c4050da36
Axel Lefaux <alefaux@lamobilery.fr>
2022-12-10 21:54:43 +0100

    Initial Commit

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by Axel Lefaux on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
